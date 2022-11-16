3dec78e218eabf04d97b53059de170d64e1d98ca
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2022-11-16 16:40:22 -0500

    changed admin UI access rules

--- Brass-Mart-API/keystone.ts
+++ Brass-Mart-API/keystone.ts
@@ -35,7 +35,7 @@ export default withAuth(
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
       // For our starter, we check that someone has session data before letting them see the Admin UI.
-      isAccessAllowed: (context) => !!context.session?.data,
+      isAccessAllowed: (context) => true /*!!context.session?.data*/,
     },
     lists,
     extendGraphqlSchema: schema =>
