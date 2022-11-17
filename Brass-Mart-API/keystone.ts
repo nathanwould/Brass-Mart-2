0354f1ca9a83af835a5667e36b838d9a6e5f8c63
Nathan Wood <nathanwood@Nathans-Mac-mini.local>
2022-11-17 11:45:24 -0500

    added port

--- Brass-Mart-API/keystone.ts
+++ Brass-Mart-API/keystone.ts
@@ -7,6 +7,8 @@ import checkout from './extendedSchema/mutations/checkout';
 
 const graphql = String.raw;
 
+const PORT = parseInt(process.env.PORT!) || 3000;
+
 const frontEndURL = process.env.FRONTEND_URL || "http://localhost:7777";
 
 const databaseURL = process.env.DATABASE_URL || 'postgres://admin:adminpassword@localhost/brassmart2';
@@ -15,6 +17,7 @@ export default withAuth(
   // Using the config function helps typescript guide you to the available options.
   config({
     server: {
+      port: PORT,
       cors: {
         origin: [frontEndURL],
         credentials: true,
